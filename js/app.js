2077b8e7474a448bf89b3180fd378b554f02b698
Danielle Boczkowski <38707471+DanielleBK@users.noreply.github.com>
2018-08-06 03:46:36 -0300

    Final
    .

--- js/app.js
+++ js/app.js
@@ -1,42 +1,76 @@
 // Add your JavaScript
-// $(document).ready(function() {
-//   $('.home').slideUp( 300 ).delay( 800 ).fadeIn( 400 );
-// });
-
-// <Splash>
 $(document).ready(function() {
-  $('.home').delay('3000').fadeIn('slow')
-
-  $.each(restaurantes, function (index, restaurante) {
-    $("<img>").attr("src", restaurante.image).appendTo(".vitrine").addClass(restaurante.name);
-
-    // $(".input").focusout(function(){
-    //     var busca = $("this").val();
-    //     alert(busca);
-    // }); Valor do que foi digitado
-})
+  $('.box-2').delay('2000').fadeIn('slow')
 });
-// <Map>
-var map;
-  function initMap() {
- map = new google.maps.Map(document.getElementById('map'), {
-   zoom: 15,
-   center: new google.maps.LatLng(-23.5578108, -46.6625469),
-   mapTypeId: 'roadmap'
- });
- // Pegando dados Json
- var features = restaurantes.map(function(r){
-  return {
-      position: new google.maps.LatLng(r.latitude, r.longitude),
-      type: 'info'
+  function showRestaurants(restaurantsArray) {
+    $('#img-restaurant').html('');
+    $.each(restaurantsArray, function (index, restaurantes) {
+      $('<img>').attr('src', restaurantes.image).attr('id', restaurantes.name).on('click', openModal).addClass(restaurantes.type).appendTo('#img-restaurant');
+  })
+}
+// MODAL
+function openModal(event) {
+  var restaurantName = $(event.target).attr('id');
+  var restaurantData =
+    $(restaurantes).filter(function (index, restaurantes) {
+      if (restaurantName === restaurantes.name) {
+        return true;
+      } else {
+        return false;
+      }
+      })[0];
+      $('#modal-restaurant-name').text(restaurantData.name);
+      $('#modal-restaurant-type').text(restaurantData.type);
+      $('#modal-restaurant-description').text(restaurantData.description);
+      $('#modal-restaurant-image').attr('src', restaurantData.image);
+      $('#modal-restaurant').modal('show');
+      $('.form-control').val('');
+      showRestaurants(restaurantes);
     }
-});
-// Criando os pontos.
+// BUSCA RESTAURANTES
+  function search() {
+    $('.form-control').keyup(function() {
+      var search = $(this).val().toLowerCase();
+      var filteredRestaurants =
+        $(restaurantes).filter(function (index, restaurant) {
+          if (search === '') {
+            return true;
+          }
+          if (restaurant.type.toLowerCase() === search) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+        $('#img-restaurant').html('');
+        showRestaurants(filteredRestaurants);
+      });
+    }
+    $(document).ready(function() {
+    showRestaurants(restaurantes);
+    search();
+  });
+//MAP
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 14,
+  center: new google.maps.LatLng(-23.5578108, -46.6625469),
+  mapTypeId: 'roadmap'
+  });
+
+
+  var features = restaurantes.map(function(r){
+  return {
+    position: new google.maps.LatLng(r.latitude, r.longitude),
+    type: 'info'
+    }
+  });
+
+// MARCAÇÕES
   features.forEach(function(feature) {
     var marker = new google.maps.Marker({
       position: feature.position,
-      // icon: icons[feature.type],
       map: map
     });
   });
-}
+  }
